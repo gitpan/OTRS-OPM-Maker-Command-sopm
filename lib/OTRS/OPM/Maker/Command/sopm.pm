76cf65c60e7b374848853e594a3de2f8f20004e7
Renee Baecker <cpan@perl-services.de>
2013-09-11 12:21:02 +0000

    Import of RENEEB/OTRS-OPM-Maker-Command-sopm-1.17 from CPAN.
    gitpan-cpan-distribution: OTRS-OPM-Maker-Command-sopm
    gitpan-cpan-version:      1.17
    gitpan-cpan-path:         RENEEB/OTRS-OPM-Maker-Command-sopm-1.17.tar.gz
    gitpan-cpan-author:       RENEEB
    gitpan-cpan-maturity:     released

--- lib/OTRS/OPM/Maker/Command/sopm.pm
+++ lib/OTRS/OPM/Maker/Command/sopm.pm
@@ -16,7 +16,7 @@ use XML::LibXML::PrettyPrint;
 
 use OTRS::OPM::Maker -command;
 
-our $VERSION = 1.16;
+our $VERSION = 1.17;
 
 sub abstract {
     return "build sopm file based on metadata";
@@ -418,7 +418,7 @@ OTRS::OPM::Maker::Command::sopm - Build .sopm file based on metadata
 
 =head1 VERSION
 
-version 1.16
+version 1.17
 
 =head1 CONFIGURATION
 
